bff6da10f9b8d3843c4e63f2224ee255f2d024dd
midhunkalarikkal <91378594+midhunkalarikkal@users.noreply.github.com>
2024-10-20 21:33:27 +0530

    useContext example, updating data within another component

--- App.js
+++ App.js
@@ -1,19 +1,28 @@
-import {React , useState} from 'react'
-import ReactDOM from 'react-dom/client'
-import { Counter } from './src/components/Counter'
-import { UserDetails } from './src/components/UserDetails'
-import { CounterTwo} from './src/components/CounterTwo'
-import {createBrowserRouter , RouterProvider, Outlet , Link} from 'react-router-dom';
+import React, { lazy, Suspense, useContext , useState , useEffect } from "react";
+import ReactDOM from "react-dom/client";
+import { Counter } from "./src/components/Counter";
+import { UserDetails } from "./src/components/UserDetails";
+import {
+  createBrowserRouter,
+  RouterProvider,
+  Outlet,
+  Link,
+} from "react-router-dom";
 
-import FirstClass from './src/components/FirstClass';
-import SecondClass from './src/components/SecondClass';
-import ThirdClass from './src/components/ThirdClass';
+import FirstClass from "./src/components/FirstClass";
+import SecondClass from "./src/components/SecondClass";
+import ThirdClass from "./src/components/ThirdClass";
 
-import Grocery from './src/components/Grocery';
+import Grocery from "./src/components/Grocery";
+
+import UserContext from "./src/utils/UserContext";
+
+import Input from "./src/components/Input";
+
+const Grocery = lazy(() => import("./src/components/Grocery"));
 
 // Sample heading
 // const heading = React.createElement("h1", {id : "heading"}, "Hello world from React!")
-
 
 // Sample react.createElement
 /*
@@ -58,35 +67,36 @@ import Grocery from './src/components/Grocery';
 //     ])]
 // )
 
-
-
 // Using jsx instead of Core react
 const jsxHeading = <h1>Hi this is Midhun K Paniker</h1>;
 
 /// Functional component \\\
 // type 1 using normal function
-const Heading = function(){
-    return(
-        <h1>This is the functional component using noraml function</h1>
-    )
-}
+const Heading = function () {
+  return <h1>This is the functional component using noraml function</h1>;
+};
 
 // type 2 using return and curly
 const HeadingOne = () => {
-    return <h1>This is functional component using return and curly brace</h1>
-}
+  return <h1>This is functional component using return and curly brace</h1>;
+};
 
 // type 3 without return and curly
-const HeadingTwo = () => <h1>This is functional component without using return and curly brace</h1>;
+const HeadingTwo = () => (
+  <h1>This is functional component without using return and curly brace</h1>
+);
 
 // type 4 without return and curly wrapped in a parenthesis
 const HeadingThree = () => (
-    <div>
-        {Heading()}
-        <HeadingOne></HeadingOne>
-        <HeadingTwo/>
-        <h1>This is functional component without using return and curly brace but wrap in a parenthesis</h1>
-    </div>
+  <div>
+    {Heading()}
+    <HeadingOne></HeadingOne>
+    <HeadingTwo />
+    <h1>
+      This is functional component without using return and curly brace but wrap
+      in a parenthesis
+    </h1>
+  </div>
 );
 
 // `Header Component` from scratch using `Functional Component` with JSX
@@ -97,108 +107,140 @@ const HeadingThree = () => (
 
 //sample css object
 const navbarBackgroundColor = {
-    backgroundColor: "#2c3e50"
-}
+  backgroundColor: "#2c3e50",
+};
 
 const Navbar = () => {
-    return (
-        <nav className="navbar" style={navbarBackgroundColor}>
-            <div className="logo">
-                <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRKBhwWT_lkLxqE389PVpukWurM-fT4vMdqBA&s" alt="No image" className="image"/>
-            </div>
-            <div className="searchBar" style={{ backgroundColor: "#2c3e50"}}>
-                <input type="text" className="input"/>
-                <button className="button">Search</button>
-            </div>
-            <div className="userIcon">
-                <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTPQHstFutlfl8tgZAtY8nDWucSWEvFM5AETQ&s" alt="" className="userIcon"/>
-            </div>
-            <div>
-                <Link to="/nextpage">Next Page</Link>
-            </div>
-            <div>
-                <Link to='/classcom'>Class Component</Link>
-            </div>
-            <div>
-                <Link to="/grocery">Grocery</Link>
-            </div>
-            <div>
-                <Link to="/users">Users</Link>
-            </div>
-            <div>
-                <Link to='/counter'>Counter</Link>
-            </div>
-        </nav>
-    )
-}
+  const {userName} = useContext(UserContext)
+  return (
+    <nav className="navbar" style={navbarBackgroundColor}>
+      <div className="logo">
+        <img
+          src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRKBhwWT_lkLxqE389PVpukWurM-fT4vMdqBA&s"
+          alt="No image"
+          className="image"
+        />
+      </div>
+      <div className="searchBar" style={{ backgroundColor: "#2c3e50" }}>
+        <input type="text" className="input" />
+        <button className="button">Search</button>
+      </div>
+      <div className="userIcon">
+        <img
+          src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTPQHstFutlfl8tgZAtY8nDWucSWEvFM5AETQ&s"
+          alt=""
+          className="userIcon"
+        />
+      </div>
+      <div>
+        <Link to="/nextpage">Next Page</Link>
+      </div>
+      <div>
+        <Link to="/classcom">Class Component</Link>
+      </div>
+      <div>
+        <Link to="/grocery">Grocery</Link>
+      </div>
+      <div>
+        <Link to="/users">Users</Link>
+      </div>
+      <div>
+        <Link to="/counter">Counter</Link>
+      </div>
+      <div>
+        <Link to="/input">Input</Link>
+      </div>
+      <div>
+        <Link>{userName}</Link>
+      </div>
+    </nav>
+  );
+};
 
 const users = [
-    { id: 1, name: "Alice", emailId: "alice@example.com" },
-    { id: 2, name: "Bob", emailId: "bob@example.com" },
-    { id: 3, name: "Charlie", emailId: "charlie@example.com" },
-    { id: 4, name: "David", emailId: "david@example.com" }
+  { id: 1, name: "Alice", emailId: "alice@example.com" },
+  { id: 2, name: "Bob", emailId: "bob@example.com" },
+  { id: 3, name: "Charlie", emailId: "charlie@example.com" },
+  { id: 4, name: "David", emailId: "david@example.com" },
 ];
 
-const DetailBox = ({userData}) => {
-    const {name , emailId} = userData;
-    return(
-        <div className="profile_card">
-            <h1>{name}</h1>
-            <p>{emailId}</p>
-        </div>
-    )
-}
-
+const DetailBox = ({ userData }) => {
+  const { name, emailId } = userData;
+  return (
+    <div className="profile_card">
+      <h1>{name}</h1>
+      <p>{emailId}</p>
+    </div>
+  );
+};
 
 const AppBody = () => {
-    return (
-        <section className="AppBody">
-            <Navbar/>
-            <Outlet />
-        </section>
-    )
-}
+
+  const [userInfo, setUserInfo] = useState()
+
+  useEffect(() => {
+    const data = {
+      name : "Midhun"
+    }
+    setUserInfo(data.name)
+  },[])
+
+  return (
+    <UserContext.Provider value={{ userName : userInfo , setUserInfo }}>
+    <section className="AppBody">
+      <Navbar />
+      <Outlet />
+    </section>
+    </UserContext.Provider>
+  );
+};
 
 const appRouter = createBrowserRouter([
-    {
-        path : '/',
-        element : <AppBody />,
-        children : [
-            {
-                path : '/nextpage',
-                element :  <UserDetails />
-            },
-            {
-                path : '/classcom',
-                element : <FirstClass name={"First"}/>
-            },
-            {
-                path : '/grocery',
-                element : <Grocery/>
-            },
-            {
-                path : '/counter',
-                element : <Counter />
-            },
-            {
-                path : '/users',
-                element :  users.map((user) =>(
-                    <DetailBox key={user.id} userData = {user}/>
-                ))
-            }
-        ]
-    }
-])
-
+  {
+    path: "/",
+    element: <AppBody />,
+    children: [
+      {
+        path: "/nextpage",
+        element: <UserDetails />,
+      },
+      {
+        path: "/classcom",
+        element: <FirstClass name={"First"} />,
+      },
+      {
+        path: "/grocery",
+        element: (
+          <Suspense fallback={<div>Loading...</div>}>
+            <Grocery />
+          </Suspense>
+        ),
+      },
+      {
+        path: "/counter",
+        element: <Counter />,
+      },
+      {
+        path : '/input',
+        element: <Input />
+      },
+      {
+        path: "/users",
+        element: users.map((user) => (
+          <DetailBox key={user.id} userData={user} />
+        )),
+      },
+    ],
+  },
+]);
 
 /// Creating the root \\\
-const root = ReactDOM.createRoot(document.getElementById("root"))
+const root = ReactDOM.createRoot(document.getElementById("root"));
 
 ///  Rendering the element \\\
 
 // jsx element rendering
-root.render(jsxHeading)
+root.render(jsxHeading);
 
 // functional component rendering
-root.render(<RouterProvider  router={appRouter}/>)
-
+root.render(<RouterProvider router={appRouter} />);
